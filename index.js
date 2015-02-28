f5b0f85ec3e7f15e6f71af8f8e17ccd745095f12
Houssem BELHADJ AHMED <houssem@belhadjahmed.com>
2015-02-28 23:41:30 +0100

    In love with docker ! final test for automatic deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Docker World! I\'m automagically deployed!!!')
+  response.send('Hello Docker World! I\'m automagically deployed ! It works ! ')
 })
 
 app.listen(app.get('port'), function() {
